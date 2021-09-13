535e728fb44b7e04ee92a7c07943ce51115e81b2
burakturkmen <Brktrkmn96>
2021-09-13 18:33:37 +0200

    Abstract Class eklendi

--- src/main/java/PageObjectModel/Abstract.java
+++ src/main/java/PageObjectModel/Abstract.java
@@ -23,10 +23,7 @@ public class Abstract {
         wait.until(ExpectedConditions.elementToBeClickable(clickElement));
         clickElement.click();
 
-
-
     }
-
 
     public void actionsMethod(WebElement targetElement){
 
@@ -51,4 +48,83 @@ public class Abstract {
         System.out.println("Tested URL  " + driver.getCurrentUrl());
 
     }
+
+    public void sendKeysFunction(WebElement sendKeysElement, String value) {
+
+        wait.until(ExpectedConditions.visibilityOf(sendKeysElement));
+
+        sendKeysElement.sendKeys(value);
+
+
+    }
+
+    public void verifyCreated(List<WebElement> tableList, String value) {
+
+        boolean result = false;
+
+        for (int i = 0; i < tableList.size(); i++) {
+            if (tableList.get(i).getText().trim().equalsIgnoreCase(value)) {
+
+                result = true;
+                System.out.println(value + " is displayed.");
+                break;
+            }
+        }
+        if (!result) {
+            Assert.fail();
+        }
+    }
+
+    //                              hangi listeden silincek?   -  hangi delete butonu?    - değer ne ?     - Yes butonu
+    public void deleteFunctionality(List<WebElement> name, List<WebElement> deleteButton, String value, WebElement buttonYes) throws InterruptedException {
+        Thread.sleep(1000);
+
+        try {
+            for (int i = 0; i < name.size(); i++) {
+
+                if (name.get(i).getText().equals(value)) {
+
+                    clickFunction(deleteButton.get(i));
+                    clickFunction(buttonYes);
+                }
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //                                          nereyi check edicez ?  -    silinen değer ne ?
+    public void verifyDeletedAbstractClass(List<WebElement> tableList, String value) throws InterruptedException {
+        Thread.sleep(1000);
+        boolean result = false;
+        for (int i = 0; i < tableList.size(); i++) {
+            if (tableList.get(i).getText().trim().equalsIgnoreCase(value)) {
+                result = true;
+                break;
+            }
+        }
+        if (result) {
+            Assert.fail();
+        } else {
+            System.out.println(value + " is not displayed");
+        }
+    }
+
+    public int randomGenerator(int max){
+
+        Random random = new Random();
+        int randomNum= random.nextInt(max);
+
+        return randomNum;
+    }
+
+    public void handleDropdown(WebElement dropdown, List<WebElement> dropdownOptions){
+
+        clickFunction(dropdown);
+
+        int randomNum =randomGenerator(dropdownOptions.size());
+
+        clickFunction(dropdownOptions.get(randomNum));
+    }
+
 }
